4860e8ebc0f5167b1e4104db8c59d4ba0c33d8ea
anshu kumari <kumarianshu2436@gmail.com.noreply.github.com>
2025-08-15 22:58:33 +0530

    loops questions solution

---
+++ loopsquestionsolution.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class loopsquestionsolution {
+    public static void main(String[] args) {
+
+//  Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+
+        // Scanner sc = new Scanner(System.in);
+        // System.err.println("Enter the first number:");
+        // int a = sc.nextInt();
+        // System.err.println("Enter the second number:");
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // if (sum % 2 == 0) {
+        //     System.out.println("Even");
+        // } else {
+        //     System.out.println("Odd");
+        // }
+
+
+
+// Write a program to find the factorial of any number entered by the user.
+
+
+    //     Scanner sc = new Scanner(System.in);
+    //     System.err.println("Enter the number:");
+    //     int n = sc.nextInt();
+    // long factorial = 1;
+    //     for (int i = 1; i <= n; i++) {
+    //         factorial *= i; // Factorial nikalne ka formula
+    //     }
+    //     System.out.println("Factorial of " + n + " is: " + factorial); // Factorial print karo
+
+
+// Question: Write a program to print the multiplication table of a number N, entered by the user
+    
+
+        // Scanner sc = new Scanner(System.in);
+        // System.err.println("Enter the number for multiplication table:");
+        // int n=sc.nextInt();
+        
+        // for (int i = 1; i<=10; i++){
+        //     System.err.println(n + " * " + i + " = " + (n * i)); // Multiplication table print karte hain
+        // }
+    
+
+
+
+
+
+    }
+
+}
