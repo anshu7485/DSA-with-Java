ca60ab5b4ea5bd100f609cbd06d8fe69c1a79722
anshu kumari <kumarianshu2436@gmail.com.noreply.github.com>
2025-08-15 09:12:09 +0530

    Max Sub Array Sum

---
+++ MaxSubArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class MaxSubArray {
+
+    public static void maxsubarraysum(int numbers[]) {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = start; j < numbers.length; j++) {
+                int end = j;
+                currentSum = 0; // Reset current sum for new subarray
+                for (int k = start; k <= end; k++) {
+                    currentSum += numbers[k];
+                }
+                System.out.println("Current Sum: " + " " + currentSum + ", ");
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+            }
+        }
+        System.out.println("Final Max Sum: " + maxSum);
+
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        maxsubarraysum(numbers);
+
+    }
+}
