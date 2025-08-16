0d6a318776ff564299869bd523f6b18e5639d49f
anshu kumari <kumarianshu2436@gmail.com.noreply.github.com>
2025-08-16 11:32:05 +0530

    selection sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -24,14 +24,41 @@ public class BubbleSort {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+
+}
+
+    // Selection Sort implementation
+
+
+    public static void selectionSort(int arr[]){
+
+        for (int i =0; i<arr.length-1; i++){
+            int minpos= i;
+            for (int j=i+1; j<arr.length; j++){
+                if (arr[minpos]>arr[j]){
+
+                    minpos=j;
+                }
+
+            }
+            
+
+        int temp = arr[minpos];
+        arr[minpos] = arr[i];
+        arr[i] = temp;
+
     }
+}
+
 
     public static void main(String[] args) {
 
         // Example array to sort
         int arr[] = { 5, 4, 1, 3, 2 };
-        bubbleSort(arr);
-        printArray(arr);
+        // bubbleSort(arr);
+        // printArray(arr);
 
+        selectionSort(arr);
+        printArray(arr);
     }
 }
