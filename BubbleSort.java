3e0d7547d68305074720273c56d00762e840eb6b
anshu kumari <kumarianshu2436@gmail.com.noreply.github.com>
2025-08-16 11:21:15 +0530

    bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,37 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+
+        // Outer loop for number of passes
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+
+            // Inner loop for comparing adjacent elements
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap if the element found is greater than the next element
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // Example array to sort
+        int arr[] = { 5, 4, 1, 3, 2 };
+        bubbleSort(arr);
+        printArray(arr);
+
+    }
+}
